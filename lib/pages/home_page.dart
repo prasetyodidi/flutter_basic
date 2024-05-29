4f0d52d8fae1bd5d48276bb99400ff7175a95ddf
Didi Prasetyo <didiprasetyo360@gmail.com>
2024-05-29 23:15:15 +0700

    feat: list view

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,80 +9,83 @@ class HomePage extends StatelessWidget {
       backgroundColor: backgroundColor,
       body: SafeArea(
           child: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 50),
-            Text(
-              'Profile Picture',
-              style: TextStyle(
-                  color: primaryColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600),
-            ),
-            SizedBox(height: 50),
-            Image.asset(
-              'assets/images/pic.png',
-              width: 140,
-              height: 140,
-              fit: BoxFit.fill,
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Anne margareta',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-            ),
-            SizedBox(height: 4),
-            Text(
-              'UX Desainer',
-              style: TextStyle(color: greyColor, fontSize: 16),
-            ),
-            SizedBox(height: 70),
-            Wrap(
-              spacing: 38,
-              runSpacing: 40,
-              children: [
-                Image.asset(
-                  'assets/images/pic.png',
-                  width: 80,
-                ),
-                Image.asset(
-                  'assets/images/pic.png',
-                  width: 80,
-                ),
-                Image.asset(
-                  'assets/images/pic.png',
-                  width: 80,
-                ),
-                Image.asset(
-                  'assets/images/pic.png',
-                  width: 80,
-                ),
-                Image.asset(
-                  'assets/images/pic.png',
-                  width: 80,
-                ),
-                Image.asset(
-                  'assets/images/pic.png',
-                  width: 80,
-                ),
-              ],
-            ),
-            SizedBox(height: 70),
-            Container(
-              width: 224,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: (){},
-                child: Text('Update Profile', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: whiteColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Text(
+                'Profile Picture',
+                style: TextStyle(
+                    color: primaryColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 50),
+              Image.asset(
+                'assets/images/pic.png',
+                width: 140,
+                height: 140,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Anne margareta',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 4),
+              Text(
+                'UX Desainer',
+                style: TextStyle(color: greyColor, fontSize: 16),
+              ),
+              SizedBox(height: 70),
+              Wrap(
+                spacing: 38,
+                runSpacing: 40,
+                children: [
+                  Image.asset(
+                    'assets/images/pic.png',
+                    width: 80,
+                  ),
+                  Image.asset(
+                    'assets/images/pic.png',
+                    width: 80,
+                  ),
+                  Image.asset(
+                    'assets/images/pic.png',
+                    width: 80,
+                  ),
+                  Image.asset(
+                    'assets/images/pic.png',
+                    width: 80,
+                  ),
+                  Image.asset(
+                    'assets/images/pic.png',
+                    width: 80,
+                  ),
+                  Image.asset(
+                    'assets/images/pic.png',
+                    width: 80,
+                  ),
+                ],
+              ),
+              SizedBox(height: 70),
+              Container(
+                width: 224,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: (){},
+                  child: Text('Update Profile', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: whiteColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)
+                    ),
                   ),
                 ),
               ),
-            )
-          ],
+              SizedBox(height: 76,)
+            ],
+          ),
         ),
       )),
     );
