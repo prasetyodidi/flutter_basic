4ee4c54e204700d6752ceba867c1d39067be7fb9
Didi Prasetyo <didiprasetyo360@gmail.com>
2024-05-29 23:42:41 +0700

    feat: modal bottom sheet

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -73,17 +73,68 @@ class HomePage extends StatelessWidget {
                 width: 224,
                 height: 50,
                 child: ElevatedButton(
-                  onPressed: (){},
-                  child: Text('Update Profile', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),),
+                  onPressed: () {
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return Container(
+                            width: double.infinity,
+                            height: 350,
+                            padding: EdgeInsets.symmetric(vertical: 50),
+                            color: whiteColor,
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Update Photo',
+                                  style: TextStyle(
+                                      fontSize: 52,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(height: 12),
+                                Text(
+                                  'You are only able to change\nthe picture profile once',
+                                  style:
+                                      TextStyle(color: greyColor, fontSize: 18),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(height: 30),
+                                Container(
+                                  width: 224,
+                                  height: 55,
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    child: Text('Continue',
+                                        style: TextStyle(
+                                            color: whiteColor,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500)),
+                                    style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(16)),
+                                      backgroundColor: orangeColor,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          );
+                        });
+                  },
+                  child: Text(
+                    'Update Profile',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: whiteColor,
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16)
-                    ),
+                        borderRadius: BorderRadius.circular(16)),
                   ),
                 ),
               ),
-              SizedBox(height: 76,)
+              SizedBox(
+                height: 76,
+              )
             ],
           ),
         ),
